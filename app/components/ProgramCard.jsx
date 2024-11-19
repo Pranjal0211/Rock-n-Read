05015df0480bf7f0916028a5d8870edaee306257
Pranjal0211 <pranjalsharma0211@gmail.com>
2024-11-19 08:59:12 -0800

    updated prgramCard

--- app/components/ProgramCard.jsx
+++ app/components/ProgramCard.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const ProgramCard = ({ icon: Icon, title, description, iconColor }) => {
   return (
-    <div className="p-6 bg-white rounded-lg shadow-lg text-center">
+    <div className="p-6 bg-white/15 border border-gray-300 border-white rounded-lg shadow-lg text-center">
       <Icon className={`w-12 h-12 mx-auto mb-4 ${iconColor}`} />
-      <h3 className="text-2xl font-semibold text-primary">{title}</h3>
+      <h3 className="text-2xl font-semibold text-cblue">{title}</h3>
       <br />
-      <p className="text-textGray">{description}</p>
+      <p className="text-white">{description}</p>
     </div>
   );
 };
